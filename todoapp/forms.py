9ae49434ff093e6318ea94477cd63ff3a81e96d2
AndresQuiVal <quirozval.andres@hotmail.com>
2020-08-05 14:09:32 -0500

    CSS external file intergration (static files); mod of form inputs and footer div

--- todoapp/forms.py
+++ todoapp/forms.py
@@ -5,4 +5,7 @@ class ToDoForm(forms.ModelForm):
     class Meta:
         model = ToDo
         fields = ["name", "description", "date", "priority"]
-        widgets = { "description" : forms.Textarea() }
+        widgets = {
+             "description" : forms.Textarea(attrs={'placeholder': 'Enter the description'}),
+             "name" : forms.TextInput(attrs={'placeholder' : 'Enter your todo name'}),
+        }
